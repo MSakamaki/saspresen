137893a23d0987c24fcf1724a7de7ab1be56c0f4
MSakamaki <daigouzigai5@gmail.com>
2013-09-05 18:56:57 +0900

    へぇ画面の高度な座標位置送信機能実装

--- public/js/he.js
+++ public/js/he.js
@@ -11,10 +11,16 @@ var hEvent = {
 		btn.bind("vmouseup", function() {
 			btn.attr("src", "./img/heeko.png");
 		});
-		btn.bind("vclick", function() {
-			console.log('hepush');
+		btn.bind("vclick", function(e) {
+			//console.log('hepush');
+			/* 左上辺が０、右下辺が１ */
+			var _clickX = ((e.clientX - this.offsetLeft) / this.width);
+			var _clickY = ((e.clientY - this.offsetTop) / this.height);
+			console.log('X:' + _clickX + ' Y:' + _clickY);
 			ws.send(JSON.stringify({
-				type : "hepush"
+				type : "hepush",
+				clickX : _clickX,
+				clickY : _clickY
 			}));
 		});
 	}
